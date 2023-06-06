4e47924834abc892055d0293d73fe177002b2764
Fred <frederic.lechevalier@epitech.eu>
2023-06-06 17:30:44 +0200

    corrected wrong fonction calling within elevation procedure

--- ai/zappy_elevation.py
+++ ai/zappy_elevation.py
@@ -2,22 +2,22 @@ import zappy_dataStruct as zds
 import zappy_network_utils as znu
 import zappy_commands as zc
 
-def canElevate(p: zds.Player, level):
+def canElevate(p: zds.Player):
     p.stats.elevating = False
-    if (level == 1):
-        p.stats.canElevate = elevationAssess(p.inventory, 1, 1, 0, 0, 0, 0, 0)
-    if (level == 2):
-        p.stats.canElevate = elevationAssess(p.inventory, 2, 1, 1, 1, 0, 0, 0)
-    if (level == 3):
-        p.stats.canElevate = elevationAssess(p.inventory, 2, 2, 0, 1, 0, 2, 0)
-    if (level == 4):
-        p.stats.canElevate = elevationAssess(p.inventory, 4, 1, 1, 2, 0, 1, 0)
-    if (level == 5):
-        p.stats.canElevate = elevationAssess(p.inventory, 4, 1, 2, 1, 3, 0, 0)
-    if (level == 6):
-        p.stats.canElevate = elevationAssess(p.inventory, 6, 1, 2, 3, 0, 1, 0)
-    if (level == 7):
-        p.stats.canElevate = elevationAssess(p.inventory, 6, 2, 2, 2, 2, 2, 1)
+    if (p.stats.level == 1):
+        elevationAssess(p.inventory, 1, 1, 0, 0, 0, 0, 0)
+    if (p.stats.level == 2):
+        elevationAssess(p.inventory, 2, 1, 1, 1, 0, 0, 0)
+    if (p.stats.level == 3):
+        elevationAssess(p.inventory, 2, 2, 0, 1, 0, 2, 0)
+    if (p.stats.level == 4):
+        elevationAssess(p.inventory, 4, 1, 1, 2, 0, 1, 0)
+    if (p.stats.level == 5):
+        elevationAssess(p.inventory, 4, 1, 2, 1, 3, 0, 0)
+    if (p.stats.level == 6):
+        elevationAssess(p.inventory, 6, 1, 2, 3, 0, 1, 0)
+    if (p.stats.level == 7):
+        elevationAssess(p.inventory, 6, 2, 2, 2, 2, 2, 1)
 
 def elevationAssess(p: zds.Player, playersNbr, linemates, deraumeres, siburs, mendianes, phiras, thystames):
     if (p.inventory.nearbyPlayers < playersNbr):
