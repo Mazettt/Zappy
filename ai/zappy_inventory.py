fed1ea6880e3f164256c56a0c0567493c36a2c7d
Fred <frederic.lechevalier@epitech.eu>
2023-06-09 16:13:40 +0200

    fixed an issue with recent pultiple item pickup update

--- ai/zappy_inventory.py
+++ ai/zappy_inventory.py
@@ -12,30 +12,37 @@ def pickUpManager(p: zds.Player, z_c: zds.Client):
     if (p.POV.vision[0].food > 0):
         zc.take(z_c, "food\n")
         p.inventory.food += 1
+        p.POV.vision[0].food -=1
         pickUpManager(p, z_c)
     if (p.POV.vision[0].linemates > 0 and p.inventory.linemates < p.requirement.linemates):
         zc.take(z_c, "linemate\n")
         p.inventory.linemates += 1
+        p.POV.vision[0].linemates -=1
         pickUpManager(p, z_c)
     if (p.POV.vision[0].deraumeres > 0 and p.inventory.deraumeres < p.requirement.deraumeres):
         zc.take(z_c, "deraumere\n")
         p.inventory.deraumeres += 1
+        p.POV.vision[0].deraumeres -=1
         pickUpManager(p, z_c)
     if (p.POV.vision[0].siburs > 0 and p.inventory.siburs < p.requirement.siburs):
         zc.take(z_c, "sibur\n")
         p.inventory.siburs += 1
+        p.POV.vision[0].siburs -=1
         pickUpManager(p, z_c)
     if (p.POV.vision[0].mendianes > 0 and p.inventory.mendianes < p.requirement.mendianes):
         zc.take(z_c, "mendiane\n")
         p.inventory.mendianes += 1
+        p.POV.vision[0].mendianes -=1
         pickUpManager(p, z_c)
     if (p.POV.vision[0].phiras > 0 and p.inventory.phiras < p.requirement.phiras):
         zc.take(z_c, "phiras\n")
         p.inventory.phiras += 1
+        p.POV.vision[0].phiras -=1
         pickUpManager(p, z_c)
     if (p.POV.vision[0].thystames > 0 and p.inventory.thystames < p.requirement.thystames):
         zc.take(z_c, "thystame\n")
         p.inventory.thystames += 1
+        p.POV.vision[0].thystames -=1
         pickUpManager(p, z_c)
 
 def trashStones(p: zds.Player): #Not used yet, might be deleted in the future
@@ -56,17 +63,17 @@ def consumeStones(p: zds.Player):
 
 def updatRequirement(p: zds.Player):
     if (p.stats.level == 2):
-        p.requirement = zds.Tile(2, 1, 1, 1, 0, 0, 0)
+        p.requirement = zds.Tile(1, 1, 1, 1, 0, 0, 0)
     if (p.stats.level == 3):
-        p.requirement = zds.Tile(2, 2, 0, 1, 0, 2, 0)
+        p.requirement = zds.Tile(1, 2, 0, 1, 0, 2, 0)
     if (p.stats.level == 4):
-        p.requirement = zds.Tile(4, 1, 1, 2, 0, 1, 0)
+        p.requirement = zds.Tile(3, 1, 1, 2, 0, 1, 0)
     if (p.stats.level == 5):
-        p.requirement = zds.Tile(4, 1, 2, 1, 3, 0, 0)
+        p.requirement = zds.Tile(3, 1, 2, 1, 3, 0, 0)
     if (p.stats.level == 6):
-        p.requirement = zds.Tile(6, 1, 2, 3, 0, 1, 0)
+        p.requirement = zds.Tile(5, 1, 2, 3, 0, 1, 0)
     if (p.stats.level == 7):
-        p.requirement = zds.Tile(6, 2, 2, 2, 2, 2, 1)
+        p.requirement = zds.Tile(5, 2, 2, 2, 2, 2, 1)
 
 def eat(p: zds.Player):
     if (p.inventory.food > 0):
