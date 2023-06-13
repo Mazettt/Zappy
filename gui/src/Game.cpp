dfebd60668bff9f7f702f611f9e7f6fa0e3305b3
Mazettt <martin.d-herouville@epitech.eu>
2023-06-13 13:37:07 +0200

    (+) window is now in Fullscreen

--- gui/src/Game.cpp
+++ gui/src/Game.cpp
@@ -76,6 +76,7 @@ void Game::run() {
 
     MyRayLib::Music musicGame("./gui/assets/GarfieldCoolCat.mp3");
 
+    ToggleFullscreen();
     while (!this->_raylibwindow.MyWindowShouldClose() && this->_BoolCloseWin == false) {
         if (_raylibwindow.MyIsKeyPressed(KEY_P) && volumeMusic < 0.9f)
                 volumeMusic += 0.1f;
