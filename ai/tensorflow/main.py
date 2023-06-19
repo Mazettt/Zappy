8e865bfc2792adaed710e4a283407ef43b723cb8
Leoleo <leo.baldachino@epitech.eu>
2023-06-19 10:55:01 +0200

    [+] Added new bias to avoid the AI to turn too many times [+] Added bias to avoid the AI to go forward too much

--- ai/tensorflow/main.py
+++ ai/tensorflow/main.py
@@ -89,6 +89,7 @@ class DQNAgent:
 
 def train_agent(p, agent, clt):
     while True:
+        last_action = ""
         action = agent.act(p.stats.get_state())
         # get_state() returns a numpy array of the player's state
         # [0] = inventory
@@ -103,6 +104,13 @@ def train_agent(p, agent, clt):
                 agent.remember(p.stats.get_state(), action, reward, p.stats.get_state(), True)
                 break
             reward = 1
+            if (last_action == "forward"):
+                reward = -2
+            if (last_action == "right"):
+                reward = -10
+            if (last_action == "left"):
+                reward = -10
+            last_action = "forward"
             agent.reward = reward
             agent.remember(p.stats.get_state(), action, reward, p.stats.get_state(), False)
         elif action == 1:
@@ -112,6 +120,9 @@ def train_agent(p, agent, clt):
                 agent.remember(p.stats.get_state(), action, reward, p.stats.get_state(), True)
                 break
             reward = 1
+            if (last_action == "right"):
+                reward = -50
+            last_action = "right"
             agent.reward = reward
             agent.remember(p.stats.get_state(), action, reward, p.stats.get_state(), False)
         elif action == 2:
@@ -121,6 +132,9 @@ def train_agent(p, agent, clt):
                 agent.remember(p.stats.get_state(), action, reward, p.stats.get_state(), True)
                 break
             reward = 1
+            if (last_action == "left"):
+                reward = -50
+            last_action = "left"
             agent.reward = reward
             agent.remember(p.stats.get_state(), action, reward, p.stats.get_state(), False)
         elif action == 3:
