cd1cceef193da95a18486521c1af3b5ea91b032c
Mazettt <martin.d-herouville@epitech.eu>
2023-06-19 14:23:49 +0200

    (+) send plv to all guis when incanting

--- server/src/commands/ai/incantation_utils.c
+++ server/src/commands/ai/incantation_utils.c
@@ -17,11 +17,14 @@ void rankup_players(zappy_t *zappy, player_t *basePlayer)
 {
     player_t backupBasePlayer = *basePlayer;
     player_t *p = NULL;
+    int it = 0;
 
     for (int i = -1; (p = parse_players(zappy, &i, p)); p = p->next) {
-        if (can_incant(p, &backupBasePlayer))
+        if (can_incant(p, &backupBasePlayer)) {
             sdprintf(zappy, p->client->command.s, "Current level: %d\n",
                 ++p->level);
+            NOTIF_GUIS(it, send_plv(zappy, it, p));
+        }
     }
 }
 
