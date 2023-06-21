9828c2c5698b33a297e9ceae21a8832ec46b0a43
Leoleo <leo.baldachino@epitech.eu>
2023-06-21 21:40:18 +0200

    [+] Re-did elevation to level 2 [+] Cleaned bugs

--- ai/ai_leo/src/Player/Player.cpp
+++ ai/ai_leo/src/Player/Player.cpp
@@ -7,6 +7,104 @@
 
 #include "Player.hpp"
 
+void Player::go_to(int destination)
+{
+    int x = 0;
+    int y = 0;
+
+    if (destination == 0) {
+        return;
+    } else if (destination == 1) {
+        x = 1;
+        y = -1;
+    } else if (destination == 2) {
+        x = 1;
+        y = 0;
+    } else if (destination == 3) {
+        x = 1;
+        y = 1;
+    } else if (destination == 4) {
+        x = 2;
+        y = -2;
+    } else if (destination == 5) {
+        x = 2;
+        y = -1;
+    } else if (destination == 6) {
+        x = 2;
+        y = 0;
+    } else if (destination == 7) {
+        x = 2;
+        y = 1;
+    } else if (destination == 8) {
+        x = 2;
+        y = 2;
+    } else if (destination == 9) {
+        x = 3;
+        y = -3;
+    } else if (destination == 10) {
+        x = 3;
+        y = -2;
+    } else if (destination == 11) {
+        x = 3;
+        y = -1;
+    } else if (destination == 12) {
+        x = 3;
+        y = 0;
+    } else if (destination == 13) {
+        x = 3;
+        y = 1;
+    } else if (destination == 14) {
+        x = 3;
+        y = 2;
+    } else if (destination == 15) {
+        x = 3;
+        y = 3;
+    } else if (destination == 16) {
+        x = 4;
+        y = -4;
+    } else if (destination == 17) {
+        x = 4;
+        y = -3;
+    } else if (destination == 18) {
+        x = 4;
+        y = -2;
+    } else if (destination == 19) {
+        x = 4;
+        y = -1;
+    } else if (destination == 20) {
+        x = 4;
+        y = 0;
+    } else if (destination == 21) {
+        x = 4;
+        y = 1;
+    } else if (destination == 22) {
+        x = 4;
+        y = 2;
+    } else if (destination == 23) {
+        x = 4;
+        y = 3;
+    } else if (destination == 24) {
+        x = 4;
+        y = 4;
+    }
+    if (x > 0) {
+        for (int i = 0; i < x; i++) {
+            forward();
+        }
+    }
+    if (y > 0) {
+        right();
+        for (int i = 0; i < y; i++) {
+            forward();
+        }
+    } else if (y < 0) {
+        left();
+        for (int i = 0; i < y; i++) {
+            forward();
+        }
+    }
+}
+
 int Player::most_x(std::string object)
 {
     int counter = 0;
@@ -15,7 +113,6 @@ int Player::most_x(std::string object)
     int tmp = 0;
 
     for (int i = 0; i < View.size(); i++) {
-        std::cout << View[i] << std::endl;
         while (View[i].find(object) != std::string::npos) {
             counter++;
             View[i].erase(View[i].find(object), object.size());
@@ -28,6 +125,7 @@ int Player::most_x(std::string object)
             destination = i;
         }
     }
+    return (destination);
 }
 
 void Player::crave_food()
@@ -35,23 +133,87 @@ void Player::crave_food()
     while (Inventory["food"] < 15) {
         View = look();
         int destination = most_x("food");
+        if (destination == 0) {
+            forward();
+        } else {
+            go_to(destination);
+            View = look();
+            vacuum();
+            Inventory = inventory();
+        }   
     }
+    emergency = false;
+}
+
+void Player::try_level_up()
+{
+    if (level == 1) {
+        Inventory = inventory();
+        std::map<std::string, int> reqs = get_requirements();
+        for (auto it = reqs.begin(); it != reqs.end(); it++) {
+            if (it->second < 0) {
+                continue;
+            }
+            for (int i = 0; i < it->second; i++) {
+                set_object(it->first);
+            }
+        }
+        socket.sendSocket("Incantation\n");
+        std::string response = socket.receiveSocket();
+        if (response.find("ko") != std::string::npos) {
+            std::cout << "LEVEL UP FAILED" << std::endl;
+            return;
+        } else {
+            level++;
+            std::cout << response << std::endl;
+            std::cout << "LEVEL UP SUCCESS" << std::endl;
+            socket.receiveSocket();
+            return;
+        }
+    }
+}
+
+std::string Player::set_object(std::string object)
+{
+    socket.sendSocket("Set " + object + "\n");
+    std::string response = socket.receiveSocket();
+    return (response);
 }
 
 void Player::play()
 {
+    level = 1;
     View = look();
     Inventory = inventory();
     vacuum();
     while (true) {
         Inventory = inventory();
-        if (Inventory["food"] < 3)
+        if (Inventory["food"] < 5)
             emergency = true;
     
         if (emergency == true) {
             crave_food();
         }
+        if (check_lvl_up() == true) {
+            std::cout << "LEVEL UP" << std::endl;
+            try_level_up();
+        }
     }
+}
+
+bool Player::check_lvl_up()
+{
+    std::map<std::string, int> reqs = get_requirements();
+    std::map<std::string, int> inv_cpy = Inventory;
+
+    for (auto it = reqs.begin(); it != reqs.end(); it++) {
+        if (it->first == "food")
+            continue;
+        if (inv_cpy[it->first] < it->second)
+            return (false);
+        inv_cpy[it->first] -= it->second;
+    }
+    return (true);
 }
 
 void Player::display_inventory()
@@ -64,10 +226,10 @@ void Player::display_inventory()
 
 void Player::vacuum()
 {
+    View = look();
     std::string player_tile = View[0];
     std::string object;
     std::string tmp;
-
     while (player_tile[0] == ' ')
         player_tile.erase(0, 1);
     for (int i = 0; i < player_tile.size() + 1; i++) {
@@ -145,6 +307,9 @@ std::map<std::string, int> Player::inventory()
         if (msg[i] == ',' || msg[i] == ']') {
             if (tmp[0] == ' ')
                 tmp.erase(0, 1);
+            if (tmp[0] == '[')
+                tmp.erase(0, 1);
+            std::cout << tmp << std::endl;
             Inventory[tmp.substr(0, tmp.find(' '))] = std::stoi(tmp.substr(tmp.find(' ') + 1, tmp.size()));
             tmp.clear();
         } else {
