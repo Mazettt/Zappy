e0319bb841bdc81f3e7fd542c4a12375a58ba46c
Fred <frederic.lechevalier@epitech.eu>
2023-06-21 15:39:24 +0200

    overall upgrade to the AI, can walk to the direction of sound to find others, can't regroup to the exact position yet

--- ai/zappy_elevation.py
+++ ai/zappy_elevation.py
@@ -10,11 +10,11 @@ import ai.zappy_commands as zc
 import ai.zappy_inventory as zi
 import ai.zappy_network_utils as znu
 import ai.zappy_set as zs
+import ai.zappy_pathfinding as zpf
+import ai.zappy_parsing as zp
 import time
 
 def canElevate(p: zds.Player):
-    if (p.inventory.food < p.stats.level * 3):
-        return False
     if (p.inventory.linemates + p.POV.vision[0].linemates < p.requirement.linemates):
         return False
     if (p.inventory.deraumeres + p.POV.vision[0].deraumeres < p.requirement.deraumeres):
@@ -29,17 +29,19 @@ def canElevate(p: zds.Player):
         return False
     if (zs.setRitual(p) == False):
         return False
-    zc.broadcast_text(p.client, "Elevating over here!\n")
-    p.stats.elevating = True
+    p.AI.elevating = True
     return True
 
 def elevationTry(p: zds.Player):
+    zc.broadcast_text(p.client, "Elev {}->Underway\n".format(p.stats.level))
     if (zc.incantation(p.client) == "Elevation underway\n"):
-        p.stats.elevating = False
+        p.AI.elevating = False
         p.stats.level += 1
         zi.consumeStones(p)
         zi.updatRequirement(p)
-    resp = znu.multiple_recv_from_server(p.client.sock, 300)
+    resp = ""
     while (resp[:14] != "Current level:" and resp != "ko\n"):
-        p.client.buffer.append(resp)
         resp = znu.multiple_recv_from_server(p.client.sock, 7)
+        resp = zp.ElevationMessageParser(p.client, resp)
+    if (resp != "ko\n"):
+        zc.broadcast_text(p.client, "Elev {}->Done\n".format(p.stats.level - 1))
