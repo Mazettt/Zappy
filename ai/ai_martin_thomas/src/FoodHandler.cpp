d111d6b6712ced5d4c923c82fac222f0b6d45b6c
RedBoard <ott.thomas68@gmail.com>
2023-06-25 18:21:52 +0200

    (|) modify value for food logic

--- ai/ai_martin_thomas/src/FoodHandler.cpp
+++ ai/ai_martin_thomas/src/FoodHandler.cpp
@@ -14,9 +14,9 @@ using namespace my;
 FoodHandler::FoodHandler(int x, int y):
     _incantationScore(0),
     _mapSize(x * y),
-    _factorLevel(8.8),
-    _factorMapSize(1.9),
-    _factorIncantationScore(2.6)
+    _factorLevel(6.2),
+    _factorMapSize(4.8),
+    _factorIncantationScore(1.4)
     {}
 
 FoodHandler::~FoodHandler() {}
@@ -42,7 +42,7 @@ void FoodHandler::calculate(int playerLevel) {
     this->_minimumFood = ((finalResult >= 4) ? finalResult : 4);
 
     // MAXIMUM PART
-    int maximumFood = std::round(this->_minimumFood * (1 + 10.2 / (levelResult + 6.4)));
+    int maximumFood = std::round(this->_minimumFood * (1 + 8.2 / (levelResult + 8.4)));
     this->_maximumFood = maximumFood;
 }
 
